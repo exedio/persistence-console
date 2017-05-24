dc6bcfdeb948bdd411db2d4f312832cc51f404d8
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2017-05-24 13:49:32 +0200

    cope update: HashConstraint supported

--- src/com/exedio/cope/console/UniqueHashedMediaCop.java
+++ src/com/exedio/cope/console/UniqueHashedMediaCop.java
@@ -18,29 +18,28 @@
 
 package com.exedio.cope.console;
 
-import static com.exedio.cope.SchemaInfo.getColumnName;
-import static com.exedio.cope.SchemaInfo.getTableName;
-import static com.exedio.cope.SchemaInfo.newConnection;
-import static com.exedio.cope.SchemaInfo.quoteName;
+import static com.exedio.cope.console.Console_Jspm.writeError;
+import static com.exedio.cope.console.Console_Jspm.writeNotConnected;
+import static com.exedio.cope.console.Console_Jspm.writeUnknown;
 
 import com.exedio.cope.Feature;
 import com.exedio.cope.Model;
+import com.exedio.cope.Pattern;
+import com.exedio.cope.Query;
+import com.exedio.cope.SchemaInfo;
+import com.exedio.cope.TransactionTry;
 import com.exedio.cope.Type;
-import com.exedio.cope.pattern.UniqueHashedMedia;
-import com.exedio.dsmf.SQLRuntimeException;
-import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import com.exedio.cope.pattern.HashConstraint;
+import com.exedio.cope.pattern.Media;
+import com.exedio.cope.pattern.MediaPath;
 import java.util.ArrayList;
 import java.util.List;
 
-final class UniqueHashedMediaCop extends TestCop<UniqueHashedMedia>
+final class UniqueHashedMediaCop extends TestCop<HashConstraint>
 {
 	UniqueHashedMediaCop(final Args args, final TestArgs testArgs)
 	{
-		super(TAB_UNIQUE_HASHED_MEDIA, "Unique Hashed Media", args, testArgs);
+		super(TAB_UNIQUE_HASHED_MEDIA, "Hash Constraints", args, testArgs);
 	}
 
 	@Override
@@ -69,14 +68,14 @@ final class UniqueHashedMediaCop extends TestCop<UniqueHashedMedia>
 	}
 
 	@Override
-	List<UniqueHashedMedia> getItems(final Model model)
+	List<HashConstraint> getItems(final Model model)
 	{
-		final ArrayList<UniqueHashedMedia> result = new ArrayList<>();
+		final ArrayList<HashConstraint> result = new ArrayList<>();
 
 		for(final Type<?> type : model.getTypes())
 			for(final Feature feature : type.getDeclaredFeatures())
-				if(feature instanceof UniqueHashedMedia)
-					result.add((UniqueHashedMedia)feature);
+				if(feature instanceof HashConstraint)
+					result.add((HashConstraint)feature);
 
 		return result;
 	}
@@ -88,75 +87,99 @@ final class UniqueHashedMediaCop extends TestCop<UniqueHashedMedia>
 	}
 
 	@Override
-	void writeValue(final Out out, final UniqueHashedMedia media, final int h)
+	void writeValue(final Out out, final HashConstraint media, final int h)
 	{
+		final Pattern pattern = media.getData().getPattern();
 		switch(h)
 		{
 			case 0:
-				out.writeRaw("<a href=\"");
-				out.write(new MediaCop(args, media.getMedia()));
-				out.writeRaw("\">");
+				if(pattern instanceof MediaPath)
+				{
+					out.writeRaw("<a href=\"");
+					out.write(new MediaCop(args, (MediaPath)pattern));
+					out.writeRaw("\">");
+				}
+
 				out.write(media.getID());
-				out.writeRaw("</a>");
+
+				if(pattern instanceof Media)
+					out.writeRaw("</a>");
+
 				break;
 			case 1:
-				out.write(media.getMedia().getContentTypeDescription().replaceAll(",", ", "));
+				if(pattern instanceof Media)
+					out.write(((Media)pattern).getContentTypeDescription().replaceAll(",", ", "));
 				break;
 			case 2:
-				out.write(media.getMessageDigestAlgorithm());
+			{
+				final String algorithm = media.getAlgorithm();
+				out.write(algorithm);
+				final Model model = media.getType().getModel();
+				writeError(out,
+						model.isConnected() &&
+						!model.getSupportedDataHashAlgorithms().contains(algorithm));
 				break;
+			}
 			case 3:
-				out.write(getSQL(media));
+			{
+				final Query<?> query = getQuery(media);
+				if(!query.getType().getModel().isConnected())
+				{
+					out.writeRaw("not yet connected");
+					writeUnknown(out, true);
+					writeNotConnected(out, this);
+					out.writeRaw("<small>");
+					out.write(query.toString());
+					out.writeRaw("</small>");
+					break;
+				}
+				final String sql;
+				try
+				{
+					sql = SchemaInfo.total(query);
+				}
+				catch(final IllegalArgumentException e)
+				{
+					// hash is not supported
+					out.write(e.getMessage());
+					writeError(out, true);
+					out.writeRaw("<br><small>");
+					out.write(query.toString());
+					out.writeRaw("</small>");
+					break;
+				}
+				out.write(sql);
 				break;
+			}
 			default:
 				throw new RuntimeException(String.valueOf(h));
 		}
 	}
 
-	private static String getSQL(final UniqueHashedMedia field)
+	private static Query<?> getQuery(final HashConstraint field)
 	{
-		final Type<?> type = field.getType();
-		final Model model = type.getModel();
-		if(!model.isConnected())
-			return "NOT YET CONNECTED";
-
-		return
-				"SELECT COUNT(*) " +
-				"FROM "  + quoteName(model, getTableName(type)) + " " +
-				"WHERE " + quoteName(model, getColumnName(field.getHash())) + "<>" + field.getMessageDigestAlgorithm() +
-				'('      + quoteName(model, getColumnName(field.getMedia().getBody())) + ')';
+		return field.getType().newQuery(field.hashDoesNotMatchIfSupported());
 	}
 
 	@Override
-	String getID(final UniqueHashedMedia media)
+	String getID(final HashConstraint media)
 	{
 		return media.getID();
 	}
 
 	@Override
-	UniqueHashedMedia forID(final Model model, final String id)
+	HashConstraint forID(final Model model, final String id)
 	{
-		return (UniqueHashedMedia)model.getFeature(id);
+		return (HashConstraint)model.getFeature(id);
 	}
 
 	@Override
-	@SuppressFBWarnings("SQL_NONCONSTANT_STRING_PASSED_TO_EXECUTE")
-	long check(final UniqueHashedMedia media, final Model model)
+	long check(final HashConstraint media, final Model model)
 	{
-		// TODO use a check method in cope
-		try(
-			Connection con = newConnection(model);
-			Statement st = con.createStatement())
+		try(TransactionTry tx = model.startTransactionTry("Console HashConstraint " + id))
 		{
-			try(ResultSet rs = st.executeQuery(getSQL(media)))
-			{
-				rs.next();
-				return rs.getLong(1);
-			}
-		}
-		catch(final SQLException e)
-		{
-			throw new SQLRuntimeException(e, media.toString());
+			return tx.commit(
+					getQuery(media).total());
 		}
 	}
 }
