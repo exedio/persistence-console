cb17d0c2e02748032348a9df5342344846fe6e61
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2022-11-23 16:59:39 +0100

    MediaTypeCop considers @Vault Media again
    Revert "MediaTypeCop omits @Vault Media"
    
    This reverts commit 819700bb8496c2143653924df8c9a3baed9559e3.

--- src/com/exedio/cope/console/MediaTypeCop.java
+++ src/com/exedio/cope/console/MediaTypeCop.java
@@ -57,7 +57,6 @@ final class MediaTypeCop extends TestCop<Media>
 		{
 			"Verifies whether the content type of Media is consistent to their body (i.e., binary data). " +
 				"This is done by usual mime-magic.",
-			"Tests non-@Vault media only!",
 			"IMPACT: " +
 				"Failures here do cause MediaServlet to send wrong Content-Type headers in http responses, " +
 				"which may confuse browsers and cause broken web pages."
@@ -84,11 +83,7 @@ final class MediaTypeCop extends TestCop<Media>
 			for(final Feature feature : type.getDeclaredFeatures())
 			{
 				if(feature instanceof Media)
-				{
-					final Media media = (Media)feature;
-					if(media.getBody().getVaultInfo()==null) // StartsWithCondition does not support vaults
-						result.add(media);
-				}
+					result.add((Media)feature);
 			}
 		}
 		return result;
