54499e90d3d59ec0babded794e68bc0cef74aef6
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2025-11-20 10:55:14 +0100

    SchemaCopTest renamed to SchemaAnalyzeApiTest

--- testsrc/com/exedio/cope/console/SchemaAnalyzeApiTest.java
+++ testsrc/com/exedio/cope/console/SchemaAnalyzeApiTest.java
@@ -47,7 +47,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class SchemaCopTest {
+public class SchemaAnalyzeApiTest {
 
   @Test
   void testSchema() throws IOException, ApiTextException {
