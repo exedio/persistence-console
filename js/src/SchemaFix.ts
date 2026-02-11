eb8ea2c35d216d00cda0b061a229e23722e3af2e
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2026-02-11 11:23:11 +0100

    Schema: ALTER TABLEs joined

--- js/src/SchemaFix.ts
+++ js/src/SchemaFix.ts
@@ -24,9 +24,15 @@ export type FixedFixable = {
   readonly tableName?: string; // undefined for subject "table" and "sequence"
   readonly name: string;
   fix: Fix;
+  joinable?: Joinable;
 };
 
-export function workOnFixes(source: FixedFixable[]): FixedFixable[] {
+export type Joinable = "head" | "middle" | "tail";
+
+export function workOnFixes(
+  source: FixedFixable[],
+  alterTablesJoined: boolean = false,
+): FixedFixable[] {
   let result: FixedFixable[] = [];
   source.forEach((i) => {
     if (i.subject === "constraint" && i.fix.method === "modify") {
@@ -43,6 +49,34 @@ export function workOnFixes(source: FixedFixable[]): FixedFixable[] {
   result.sort((a, b) => {
     return orderIndex(a) - orderIndex(b);
   });
+  if (alterTablesJoined) {
+    let beforeWithSameTable: FixedFixable | undefined;
+    let beforeWithSameTableHead: boolean = false;
+    result.forEach((i) => {
+      // condition below matches ALTER TABLE statements
+      if (i.subject === "column" || i.subject === "constraint") {
+        if (i.tableName === beforeWithSameTable?.tableName) {
+          (beforeWithSameTable as FixedFixable).joinable =
+            beforeWithSameTableHead ? "head" : "middle";
+          i.joinable = "tail";
+          beforeWithSameTable = i;
+          beforeWithSameTableHead = false;
+        } else {
+          delete i.joinable;
+          beforeWithSameTable = i;
+          beforeWithSameTableHead = true;
+        }
+      } else {
+        delete i.joinable;
+        beforeWithSameTable = undefined;
+        beforeWithSameTableHead = false;
+      }
+    });
+  } else {
+    result.forEach((i) => {
+      delete i.joinable;
+    });
+  }
   return result;
 }
 
@@ -97,6 +131,26 @@ function orderIndex(cb: FixedFixable): number {
   throw new Error(JSON.stringify(cb));
 }
 
-export function encodePatch(java: boolean, s: string): string {
-  return java ? '"' + s.replaceAll('"', '\\"') + '",' : s + ";";
+export function encodePatch(
+  joinable: Joinable | undefined,
+  java: boolean,
+  s: string,
+): string {
+  // another part of the same statement came before me
+  if (joinable === "middle" || joinable === "tail")
+    s = s.replace(/^ALTER +TABLE *([`"])[0-9A-Za-z_]+\1 */, "");
+
+  // another part of the same statement comes after me
+  const moreToCome = joinable === "head" || joinable === "middle";
+  if (moreToCome) s = s + ",";
+
+  if (java) s = '"' + s.replaceAll('"', '\\"') + '"';
+
+  if (moreToCome) {
+    if (java) s = s + " +";
+  } else {
+    s = s + (java ? "," : ";");
+  }
+
+  return s;
 }
--- js/tests/SchemaFix.spec.ts
+++ js/tests/SchemaFix.spec.ts
@@ -1,5 +1,5 @@
 import { expect } from "vitest";
-import { type Fix, workOnFixes } from "@/SchemaFix";
+import { type Fix, type FixedFixable, workOnFixes } from "@/SchemaFix";
 
 describe("Fix", () => {
   it("should pass through empty array", async () => {
@@ -246,4 +246,211 @@ describe("Fix", () => {
       },
     ]);
   });
+  it("should join columns", async () => {
+    expect(
+      workOnFixes(
+        [
+          columnDrop("myTableName", "myName1"),
+          columnDrop("myTableName", "myName2"),
+          columnDrop("myTableName", "myName3"),
+          columnDrop("myTableName", "myName4"),
+        ],
+        true,
+      ),
+    ).toStrictEqual([
+      columnDrop("myTableName", "myName1", "head"),
+      columnDrop("myTableName", "myName2", "middle"),
+      columnDrop("myTableName", "myName3", "middle"),
+      columnDrop("myTableName", "myName4", "tail"),
+    ]);
+  });
+  it("should join columns with padding", async () => {
+    expect(
+      workOnFixes(
+        [
+          columnDrop("myTableName1", "myName1", "middle"), // check whether joinable is deleted
+          columnDrop("myTableNameJ", "myName2"),
+          columnDrop("myTableNameJ", "myName3"),
+          columnDrop("myTableName2", "myName4", "middle"), // check whether joinable is deleted
+          columnDrop("myTableNameJ", "myName5"),
+          columnDrop("myTableNameJ", "myName6"),
+          columnDrop("myTableName3", "myName7", "middle"), // check whether joinable is deleted
+        ],
+        true,
+      ),
+    ).toStrictEqual([
+      columnDrop("myTableName1", "myName1"),
+      columnDrop("myTableNameJ", "myName2", "head"),
+      columnDrop("myTableNameJ", "myName3", "tail"),
+      columnDrop("myTableName2", "myName4"),
+      columnDrop("myTableNameJ", "myName5", "head"),
+      columnDrop("myTableNameJ", "myName6", "tail"),
+      columnDrop("myTableName3", "myName7"),
+    ]);
+  });
+  it("should not join columns when disabled", async () => {
+    expect(
+      workOnFixes(
+        [
+          columnDrop("myTableName", "myName1"),
+          columnDrop("myTableName", "myName2", "middle"), // check whether joinable is deleted
+          columnDrop("myTableName", "myName3"),
+          columnDrop("myTableName", "myName4"),
+        ],
+        false,
+      ),
+    ).toStrictEqual([
+      columnDrop("myTableName", "myName1"),
+      columnDrop("myTableName", "myName2"),
+      columnDrop("myTableName", "myName3"),
+      columnDrop("myTableName", "myName4"),
+    ]);
+  });
+  it("should join columns with different methods", async () => {
+    expect(
+      workOnFixes(
+        [
+          columnDrop("myTableName", "myName1"),
+          columnAdd("myTableName", "myName2"),
+          columnDrop("myTableName", "myName3"),
+          columnAdd("myTableName", "myName4"),
+        ],
+        true,
+      ),
+    ).toStrictEqual([
+      columnDrop("myTableName", "myName1", "head"),
+      columnDrop("myTableName", "myName3", "middle"),
+      columnAdd("myTableName", "myName2", "middle"),
+      columnAdd("myTableName", "myName4", "tail"),
+    ]);
+  });
+  it("should join constraints", async () => {
+    expect(
+      workOnFixes(
+        [
+          constraintDrop("myTableName", "myName1"),
+          constraintDrop("myTableName", "myName2"),
+          constraintDrop("myTableName", "myName3"),
+        ],
+        true,
+      ),
+    ).toStrictEqual([
+      constraintDrop("myTableName", "myName1", "head"),
+      constraintDrop("myTableName", "myName2", "middle"),
+      constraintDrop("myTableName", "myName3", "tail"),
+    ]);
+  });
+  it("should join columns mixed with constraints", async () => {
+    expect(
+      workOnFixes(
+        [
+          columnDrop("myTableName", "myName1"),
+          constraintDrop("myTableName", "myName2"),
+          columnDrop("myTableName", "myName3"),
+          constraintDrop("myTableName", "myName4"),
+        ],
+        true,
+      ),
+    ).toStrictEqual([
+      constraintDrop("myTableName", "myName2", "head"),
+      constraintDrop("myTableName", "myName4", "middle"),
+      columnDrop("myTableName", "myName1", "middle"),
+      columnDrop("myTableName", "myName3", "tail"),
+    ]);
+  });
+  it("should break join by table change", async () => {
+    expect(
+      workOnFixes(
+        [
+          columnDrop("myTableName1", "myName1"),
+          columnDrop("myTableName1", "myName2"),
+          columnDrop("myTableName2", "myName3"),
+          columnDrop("myTableName2", "myName4"),
+        ],
+        true,
+      ),
+    ).toStrictEqual([
+      columnDrop("myTableName1", "myName1", "head"),
+      columnDrop("myTableName1", "myName2", "tail"),
+      columnDrop("myTableName2", "myName3", "head"),
+      columnDrop("myTableName2", "myName4", "tail"),
+    ]);
+  });
+  it("should break join by other statement", async () => {
+    expect(
+      workOnFixes(
+        [
+          columnDrop("myTableName", "myName1"),
+          columnAdd("myTableName", "myName2"),
+          {
+            subject: "table",
+            name: "myTableName",
+            fix: { method: "drop" },
+            joinable: "middle", // check whether joinable is deleted
+          },
+          columnDrop("myTableName", "myName3"),
+          columnAdd("myTableName", "myName4"),
+        ],
+        true,
+      ),
+    ).toStrictEqual([
+      columnDrop("myTableName", "myName1", "head"),
+      columnDrop("myTableName", "myName3", "tail"),
+      {
+        subject: "table",
+        name: "myTableName",
+        fix: { method: "drop" },
+      },
+      columnAdd("myTableName", "myName2", "head"),
+      columnAdd("myTableName", "myName4", "tail"),
+    ]);
+  });
 });
+
+function columnAdd(
+  tableName: string,
+  name: string,
+  joinable: "head" | "middle" | "tail" | undefined = undefined,
+): FixedFixable {
+  return {
+    subject: "column",
+    tableName,
+    name,
+    fix: { method: "add" },
+    joinable,
+  };
+}
+function columnDrop(
+  tableName: string,
+  name: string,
+  joinable: "head" | "middle" | "tail" | undefined = undefined,
+): FixedFixable {
+  if (joinable)
+    return {
+      subject: "column",
+      tableName,
+      name,
+      fix: { method: "drop" },
+      joinable,
+    };
+
+  return {
+    subject: "column",
+    tableName,
+    name,
+    fix: { method: "drop" },
+  };
+}
+function constraintDrop(
+  tableName: string,
+  name: string,
+  joinable: "head" | "middle" | "tail" | undefined = undefined,
+): FixedFixable {
+  return {
+    subject: "constraint",
+    tableName,
+    name,
+    fix: { method: "drop" },
+    joinable,
+  };
+}
