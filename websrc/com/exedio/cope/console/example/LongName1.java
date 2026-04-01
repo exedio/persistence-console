7cce3ac4654e784efce24bacf86063a63b969c89
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2026-04-01 17:53:45 +0200

    Schema: min-width for sql div

--- websrc/com/exedio/cope/console/example/LongName1.java
+++ websrc/com/exedio/cope/console/example/LongName1.java
@@ -18,20 +18,57 @@
 
 package com.exedio.cope.console.example;
 
+import com.exedio.cope.CheckConstraint;
+import com.exedio.cope.Cope;
 import com.exedio.cope.CopeSchemaName;
+import com.exedio.cope.StringField;
 
 @CopeSchemaName("Long1Loooooooooooooooooooooooooooong")
 @SuppressWarnings("StaticMethodOnlyUsedInOneClass") // OK: for example TYPE
 final class LongName1 extends AnItem
 {
+	@CopeSchemaName("looooooooooooooooooooooooooooooooongNameField")
+	static final StringField longNameField = new StringField();
+
+	@SuppressWarnings("unused") // OK: CheckConstraint
+	static final CheckConstraint longConstraint = new CheckConstraint(Cope.and(
+			longNameField.notEqual("a"),
+			longNameField.notEqual("b"),
+			longNameField.notEqual("c"),
+			longNameField.notEqual("d"),
+			longNameField.notEqual("e"),
+			longNameField.notEqual("f"),
+			longNameField.notEqual("g"),
+			longNameField.notEqual("h"),
+			longNameField.notEqual("i"),
+			longNameField.notEqual("j"),
+			longNameField.notEqual("k"),
+			longNameField.notEqual("l"),
+			longNameField.notEqual("m"),
+			longNameField.notEqual("n"),
+			longNameField.notEqual("o"),
+			longNameField.notEqual("p"),
+			longNameField.notEqual("q"),
+			longNameField.notEqual("r"),
+			longNameField.notEqual("s"),
+			longNameField.notEqual("t"),
+			longNameField.notEqual("u"),
+			longNameField.notEqual("v"),
+			longNameField.notEqual("w"),
+			longNameField.notEqual("x"),
+			longNameField.notEqual("y"),
+			longNameField.notEqual("z")
+	));
+
 	/**
 	 * Creates a new LongName1 with all the fields initially needed.
 	 * @param aField the initial value for field {@link #aField}.
 	 * @param letter the initial value for field {@link #letter}.
 	 * @param secondLetter the initial value for field {@link #secondLetter}.
 	 * @param color the initial value for field {@link #color}.
-	 * @throws com.exedio.cope.MandatoryViolationException if aField, letter, secondLetter, color is null.
-	 * @throws com.exedio.cope.StringLengthViolationException if aField violates its length constraint.
+	 * @param longNameField the initial value for field {@link #longNameField}.
+	 * @throws com.exedio.cope.MandatoryViolationException if aField, letter, secondLetter, color, longNameField is null.
+	 * @throws com.exedio.cope.StringLengthViolationException if aField, longNameField violates its length constraint.
 	 */
 	@com.exedio.cope.instrument.Generated // customize with @WrapperType(constructor=...) and @WrapperInitial
 	@java.lang.SuppressWarnings({"RedundantArrayCreation","RedundantSuppression"})
@@ -39,7 +76,8 @@ final class LongName1 extends AnItem
 				@javax.annotation.Nonnull final java.lang.String aField,
 				@javax.annotation.Nonnull final com.exedio.cope.console.example.AnItem.Letter letter,
 				@javax.annotation.Nonnull final com.exedio.cope.console.example.AnItem.Letter secondLetter,
-				@javax.annotation.Nonnull final com.exedio.cope.console.example.AnItem.Color color)
+				@javax.annotation.Nonnull final com.exedio.cope.console.example.AnItem.Color color,
+				@javax.annotation.Nonnull final java.lang.String longNameField)
 			throws
 				com.exedio.cope.MandatoryViolationException,
 				com.exedio.cope.StringLengthViolationException
@@ -49,6 +87,7 @@ final class LongName1 extends AnItem
 			com.exedio.cope.SetValue.map(com.exedio.cope.console.example.AnItem.letter,letter),
 			com.exedio.cope.SetValue.map(com.exedio.cope.console.example.AnItem.secondLetter,secondLetter),
 			com.exedio.cope.SetValue.map(com.exedio.cope.console.example.AnItem.color,color),
+			com.exedio.cope.SetValue.map(LongName1.longNameField,longNameField),
 		});
 	}
 
@@ -57,6 +96,30 @@ final class LongName1 extends AnItem
 	 */
 	@com.exedio.cope.instrument.Generated // customize with @WrapperType(genericConstructor=...)
 	private LongName1(final com.exedio.cope.SetValue<?>... setValues){super(setValues);}
+
+	/**
+	 * Returns the value of {@link #longNameField}.
+	 */
+	@com.exedio.cope.instrument.Generated // customize with @Wrapper(wrap="get")
+	@java.lang.SuppressWarnings({"FinalMethodInFinalClass","RedundantSuppression","UnnecessarilyQualifiedStaticUsage"})
+	@javax.annotation.Nonnull
+	final java.lang.String getLongNameField()
+	{
+		return LongName1.longNameField.get(this);
+	}
+
+	/**
+	 * Sets a new value for {@link #longNameField}.
+	 */
+	@com.exedio.cope.instrument.Generated // customize with @Wrapper(wrap="set")
+	@java.lang.SuppressWarnings({"FinalMethodInFinalClass","RedundantSuppression","UnnecessarilyQualifiedStaticUsage"})
+	final void setLongNameField(@javax.annotation.Nonnull final java.lang.String longNameField)
+			throws
+				com.exedio.cope.MandatoryViolationException,
+				com.exedio.cope.StringLengthViolationException
+	{
+		LongName1.longNameField.set(this,longNameField);
+	}
 
 	@com.exedio.cope.instrument.Generated
 	@java.io.Serial
