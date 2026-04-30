920429c316fae47f77062ad6261cf2d2a14737ee
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2026-04-30 14:39:06 +0200

    svelte-ignore

--- js/src/Schema.svelte
+++ js/src/Schema.svelte
@@ -460,6 +460,7 @@
 {/snippet}
 
 {#snippet bullet(bullet: Bullet)}
+  <!-- svelte-ignore a11y_consider_explicit_label -->
   <button class={["bullet", bullet.bulletColor]} disabled={true}>&nbsp;</button>
 {/snippet}
 
