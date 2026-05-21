01bc9b05c1a79d2bc926630cabc9eeb22002df32
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2026-05-21 11:23:24 +0200

    TypeIsEmptyCop added

---
+++ src/com/exedio/cope/console/TypeIsEmptyCop.java
@@ -0,0 +1,93 @@
+/*
+ * Copyright (C) 2004-2009  exedio GmbH (www.exedio.com)
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package com.exedio.cope.console;
+
+import com.exedio.cope.TransactionTry;
+import com.exedio.cope.Type;
+import java.util.ArrayList;
+import java.util.List;
+
+final class TypeIsEmptyCop extends TestCop<Type<?>> {
+
+  static final String TAB = "typeIsEmpty";
+
+  TypeIsEmptyCop(final Args args, final TestArgs testArgs) {
+    super(TAB, "Type Is Empty", args, testArgs);
+  }
+
+  @Override
+  String[] getHeadingHelp() {
+    return new String[] {
+      "There are no items of a type in database at all.",
+      "Lists all non-abstract types.",
+    };
+  }
+
+  @Override
+  protected TypeIsEmptyCop newArgs(final Args args) {
+    return new TypeIsEmptyCop(args, testArgs);
+  }
+
+  @Override
+  protected TypeIsEmptyCop newTestArgs(final TestArgs testArgs) {
+    return new TypeIsEmptyCop(args, testArgs);
+  }
+
+  @Override
+  List<Type<?>> getItems() {
+    final ArrayList<Type<?>> result = new ArrayList<>();
+
+    for (final Type<?> type : app.model.getTypes())
+      if (!type.isAbstract()) result.add(type);
+
+    return result;
+  }
+
+  @Override
+  String getID(final Type<?> type) {
+    return type.getID();
+  }
+
+  @Override
+  Type<?> forID(final String id) {
+    return app.model.getType(id);
+  }
+
+  @Override
+  List<Column<Type<?>>> columns() {
+    return COLUMNS;
+  }
+
+  private static final List<Column<Type<?>>> COLUMNS = List.of(
+    column("Type", Type::toString)
+  );
+
+  @Override
+  long check(final Type<?> type) {
+    try (
+      TransactionTry tx = app.model.startTransactionTry(
+        "Console TypeIsEmptyCop " + id
+      )
+    ) {
+      final boolean result = type.newQuery().total() == 0;
+      tx.commit();
+      return result ? 1 : 0;
+    }
+  }
+}
---
+++ testsrc/com/exedio/cope/console/TypeIsEmptyTest.java
@@ -0,0 +1,116 @@
+/*
+ * Copyright (C) 2004-2009  exedio GmbH (www.exedio.com)
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package com.exedio.cope.console;
+
+import static com.exedio.cope.console.SchemaGetApiTest.assertOrphaned;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.exedio.cope.ActivationParameters;
+import com.exedio.cope.ConnectProperties;
+import com.exedio.cope.Item;
+import com.exedio.cope.Model;
+import com.exedio.cope.Type;
+import com.exedio.cope.TypesBound;
+import com.exedio.cope.util.Sources;
+import java.util.List;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class TypeIsEmptyTest {
+
+  @Test
+  void test() {
+    final var cop = new TypeIsEmptyCop(CopTest.args(MODEL), CopTest.testArgs);
+
+    assertEquals(List.of(MyType.TYPE), cop.getItems()); // Lists all non-abstract types.
+
+    assertEquals(1, cop.check(MyType.TYPE)); // There are no items of a type in database at all.
+
+    try (var tx = MODEL.startTransactionTry("null item")) {
+      new MyType();
+      tx.commit();
+    }
+    assertEquals(0, cop.check(MyType.TYPE)); // OK
+  }
+
+  private abstract static class MyAbstractType extends Item {
+
+    @java.io.Serial
+    private static final long serialVersionUID = 1l;
+
+    static final Type<?> TYPE = TypesBound.newTypeAbstract(
+      MyAbstractType.class
+    );
+
+    MyAbstractType() {}
+
+    private MyAbstractType(final ActivationParameters ap) {
+      super(ap);
+    }
+  }
+
+  private static final class MyType extends MyAbstractType {
+
+    @java.io.Serial
+    private static final long serialVersionUID = 1l;
+
+    static final Type<?> TYPE = TypesBound.newType(MyType.class, MyType::new);
+
+    MyType() {}
+
+    private MyType(final ActivationParameters ap) {
+      super(ap);
+    }
+  }
+
+  private static final Model MODEL = new Model(
+    MyType.TYPE,
+    MyAbstractType.TYPE
+  );
+
+  @BeforeAll
+  static void connect() {
+    final java.util.Properties props = new java.util.Properties();
+    props.setProperty("connection.url", "jdbc:hsqldb:mem:copeconsoletest");
+    props.setProperty("connection.username", "sa");
+    props.setProperty("connection.password", "");
+    MODEL.connect(
+      assertOrphaned(ConnectProperties.create(Sources.view(props, "DESC")))
+    );
+  }
+
+  @AfterAll
+  static void disconnect() {
+    MODEL.disconnect();
+  }
+
+  @BeforeEach
+  void createSchema() {
+    MODEL.createSchema();
+  }
+
+  @AfterEach
+  void tearDownSchema() {
+    MODEL.rollbackIfNotCommitted();
+    MODEL.tearDownSchema();
+  }
+}
